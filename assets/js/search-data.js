3bcd7dea693022e5dea57f1df3adf391af833f99
steinate <56948539+steinate@users.noreply.github.com>
2025-05-07 09:00:09 +0000

    Deploying to gh-pages from @ steinate/steinate.github.io@b0eb561e7e40fba060e8cf6507838c90fbacface 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,18 +416,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
