1f1345379a26b5fab9c979019f341023ac2b5e87
steinate <56948539+steinate@users.noreply.github.com>
2025-05-07 08:47:08 +0000

    Deploying to gh-pages from @ steinate/steinate.github.io@541fb5b3040f1c16d7b1b49c2897cc2ae71b4f12 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-jiaqi-peng",
-    title: "Jiaqi Peng",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
@@ -44,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-happy-birthday-周丹",
+        },{id: "post-happy-birthday",
         
-          title: "Happy Birthday, 周丹!",
+          title: "Happy Birthday!",
         
         description: "",
         section: "Posts",
