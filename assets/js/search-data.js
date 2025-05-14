d83d277131b1da7ba3374d152c47f9fad7142f9a
steinate <56948539+steinate@users.noreply.github.com>
2025-05-14 10:32:00 +0000

    Deploying to gh-pages from @ steinate/steinate.github.io@0bbf2d8fc9b1c5becf0247c51eba1718391e0da0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-happy-birthday",
+        },{id: "post-hello-to-my-page",
+        
+          title: "Hello To My Page",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/hello-to-my-page/";
+          
+        },
+      },{id: "post-happy-birthday",
         
           title: "Happy Birthday!",
         
